81f5fffeb06a7cb09d0df65ed8c19db5c870e8af
Sean McDonnell <seanmcdonnell@Seans-MacBook-Pro.local>
2019-09-05 13:56:22 -0400

    Fix mistake setting port

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const server = require('./api/server.js');
 
 const port = process.env.PORT || 4000;
 
-const port = 4000;
 server.listen(port, () => {
     console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
